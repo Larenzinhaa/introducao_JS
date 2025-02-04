10294c655ae1f899c363cdf012539d65287166e9
Larenzinhaa <analaurendourado@gmail.com>
2025-02-04 12:09:02 -0300

    revisão finalizada

---
+++ exemplo/exemplo_botao/script.js
@@ -0,0 +1,6 @@
+let num1 = parseFloat(document. getElementById("numero1").value)
+let num2 = parseFloat (document.getElementById("numero2").value)
+
+let soma = num1+num2
+
+document.getElementById("resultado"). innerText = soma
---
+++ exemplo/exemplo_soma/script.js
@@ -0,0 +1,4 @@
+let num1 = parseInt (prompt ("Digite o primeiro número"))
+let num2 = parseInt (prompt("Digite o segundo número"))
+let soma = num1+num2
+document.getElementById("resultado").innerText = "A soma dos dois números é: " + soma
